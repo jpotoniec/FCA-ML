33f4fe7a73efc25265c9803bde86c07cbc4b4f63
Jędrzej Potoniec <Jedrzej.Potoniec@cs.put.poznan.pl>
2014-03-17 09:22:49 +0100

    ContextDataModel: getAttributeIndex and isAttributeColumn

--- src/main/java/put/semantic/fcanew/ui/ContextDataModel.java
+++ src/main/java/put/semantic/fcanew/ui/ContextDataModel.java
@@ -6,10 +6,7 @@
 package put.semantic.fcanew.ui;
 
 import javax.swing.table.AbstractTableModel;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import put.semantic.fcanew.Attribute;
-import put.semantic.fcanew.Implication;
 import put.semantic.fcanew.POD;
 import put.semantic.fcanew.PartialContext;
 
@@ -20,7 +17,6 @@ import put.semantic.fcanew.PartialContext;
 public class ContextDataModel extends AbstractTableModel implements PartialContext.ContextChangedListener {
 
     private PartialContext context;
-    private Implication currentImplication;
 
     public ContextDataModel(PartialContext context) {
         this.context = context;
@@ -40,10 +36,10 @@ public class ContextDataModel extends AbstractTableModel implements PartialConte
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         POD pod = context.getPODs().get(rowIndex);
-        if (columnIndex == 0) {
+        if (!isAttributeColumn(columnIndex)) {
             return pod.getId();
         }
-        Attribute a = context.getAttributes().get(columnIndex - 1);
+        Attribute a = context.getAttributes().get(getAttributeIndex(columnIndex));
         if (pod.getPositive().contains(a)) {
             return "+";
         } else if (pod.getNegative().contains(a)) {
@@ -53,12 +49,20 @@ public class ContextDataModel extends AbstractTableModel implements PartialConte
         }
     }
 
+    protected int getAttributeIndex(int column) {
+        return column - 1;
+    }
+
+    protected boolean isAttributeColumn(int column) {
+        return getAttributeIndex(column) >= 0;
+    }
+
     @Override
     public String getColumnName(int column) {
-        if (column == 0) {
+        if (!isAttributeColumn(column)) {
             return "Individual";
         }
-        return context.getAttributes().get(column - 1).toString();
+        return context.getAttributes().get(getAttributeIndex(column)).toString();
     }
 
     @Override
@@ -68,58 +72,16 @@ public class ContextDataModel extends AbstractTableModel implements PartialConte
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return columnIndex > 0;
+        return isAttributeColumn(columnIndex);
     }
-
-    //drop it. maybe do filtering. maybe don't
-//    public void setCurrentImplication(Implication currentImplication) {
-//        this.currentImplication = currentImplication;
-//        fireTableDataChanged();
-//    }
-//
-//    public enum PODState {
-//
-//        NORMAL, IN_PREMISE, IN_CONCLUSION, IN_BOTH
-//    };
-//
-//    public PODState getState(OWLNamedIndividual ind) {
-//        if (currentImplication == null) {
-//            return PODState.NORMAL;
-//        }
-//        boolean p = false, c = false;
-//        POD pod = null;
-//        for (POD x : context.getPODs()) {
-//            if (x.getId() == ind) {
-//                pod = x;
-//            }
-//        }
-//        if (pod == null) {
-//            return PODState.NORMAL;
-//        }
-//        System.err.printf("%s %s %s\n", pod.getId().getIRI(), currentImplication.getPremises().toString(), pod.getPositive());
-//        if (pod.getPositive().containsAll(currentImplication.getPremises())) {
-//            p = true;
-//        }
-//        if (pod.getPositive().containsAll(currentImplication.getConclusions())) {
-//            c = true;
-//        }
-//        if (p && c) {
-//            return PODState.IN_BOTH;
-//        } else if (p) {
-//            return PODState.IN_PREMISE;
-//        } else if (c) {
-//            return PODState.IN_CONCLUSION;
-//        }
-//        return PODState.NORMAL;
-//    }
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        if (columnIndex == 0) {
+        if (!isAttributeColumn(columnIndex)) {
             throw new IllegalArgumentException();
         }
         POD pod = context.getPODs().get(rowIndex);
-        Attribute a = context.getAttributes().get(columnIndex - 1);
+        Attribute a = context.getAttributes().get(getAttributeIndex(columnIndex));
         System.out.printf("'%s'\n", aValue.toString());
         switch (aValue.toString()) {
             case "+":
